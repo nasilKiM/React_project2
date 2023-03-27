83b4eb9cb51efd33aac4b5b6672fdd6797facfbb
nasilKiM <nasikun@naver.com>
2023-03-27 23:58:35 +0900

    DELETE: RECOIL SETTING

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 import router from 'Routes/routing';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'Styles/global';
 import { theme } from 'Styles/theme';
 
 function App() {
-	const queryClient = new QueryClient();
-
 	return (
-		<QueryClientProvider client={queryClient}>
-			<RecoilRoot>
-				<ThemeProvider theme={theme}>
-					<GlobalStyles />
-					<RouterProvider router={router} />
-				</ThemeProvider>
-			</RecoilRoot>
-		</QueryClientProvider>
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	);
 }
 
